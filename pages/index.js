828c3d425e8dfc40bf4ba91fea74a72ed3ea006d
Matheus Yuri Silva <80913079+MatheusYurirs@users.noreply.github.com>
2025-09-17 20:32:31 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home(){
+    return  (
+        <div>
+    <h1>Te amo, meu amor</h1>
+    <p>De todo coração do mozinho nha</p>
+    <p>E aqui mostra o meu esforço enquanto estudo para me tornar um ótimo dev</p>
+    </div>
+    
+);
+}
+
+export default Home;
