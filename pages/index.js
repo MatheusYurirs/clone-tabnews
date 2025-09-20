d06d73b72baf5b78dee72f12a73a208aa17f2445
Matheus Yuri Silva <80913079+MatheusYurirs@users.noreply.github.com>
2025-09-20 13:56:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-function Home(){
-    return  (
-        <div>
-    <h1>Te amo, meu amor</h1>
-    <p>De todo coração do mozinho nha</p>
-    <p>E aqui mostra o meu esforço enquanto estudo para me tornar um ótimo dev! 🥰​</p>
+function Home() {
+  return (
+    <div>
+      <h1>Te amo, meu amor</h1>
+      <p>De todo coração do mozinho nha</p>
+      <p>
+        E aqui mostra o meu esforço enquanto estudo para me tornar um ótimo dev!
+        🥰​
+      </p>
     </div>
-    
-);
+  );
 }
 
 export default Home;
